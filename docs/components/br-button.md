e8ba7979c87763fed102aee13232078b3ad01b87
Diogo Coelho <diogooliveiracoelho@gmail.com>
2023-05-12 01:10:50 -0300

    Início da configuração do componente BrButton

--- docs/components/br-button.md
+++ docs/components/br-button.md
@@ -5,11 +5,35 @@ import BrButton from '../../src/components/button/BrButton.vue'
 # BrButton
 
 
-`Button` é um componente.
-<BrButton>Isso é um botão</BrButton>
+`Button` é um componente que representa um elemento clicável que retornará uma ação para o usuário.
+<BrButton>Button</BrButton>
 
 ```vue
-	<BrButton> Isso é um botão </BrButton>
+<BrButton> Button </BrButton>
+```
+
+## Exemplos
+
+### Variantes
+
+Passe o atributo `variant` para configurar a variante do botão.
+
+<div class="inline-flex w-full">
+	<BrButton class="mr-x-small">Default</BrButton>
+	<BrButton class="mr-x-small" variant="primary">Primary</BrButton>
+	<BrButton class="mr-x-small" variant="neutral">Neutral</BrButton>
+	<BrButton class="mr-x-small" variant="success">Success</BrButton>
+	<BrButton class="mr-x-small" variant="warning">Warning</BrButton>
+	<BrButton variant="danger">Danger</BrButton>
+</div>
+
+```vue 
+<BrButton>Default</BrButton>
+<BrButton variant="primary">Primary</BrButton>
+<BrButton variant="neutral">Neutral</BrButton>
+<BrButton variant="success">Success</BrButton>
+<BrButton variant="warning">Warning</BrButton>
+<BrButton variant="danger">Danger</BrButton>
 ```
 
 <style lang="sass">
--- src/components/button/BrButton.vue
+++ src/components/button/BrButton.vue
@@ -1,14 +1,64 @@
 <template>
-  <button class="br-button" @click="onClick">
+  <button
+    v-bind="$attrs"
+    :class="rootClasses"
+    :type="type"
+    :disabled="computedDisabled"
+    @click="onClick"
+  >
     <slot />
   </button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { ButtonHTMLAttributes, defineComponent, PropType } from 'vue'
 
 export default defineComponent({
   name: 'BrButton',
+  inheritAttrs: false,
+  props: {
+    /**
+     * disabled status
+     * @values true, false
+     */
+    disabled: {
+      type: Boolean,
+    },
+    /**
+     * Color of button
+     * @values primary, secondary, info, warning, danger
+     */
+    variant: {
+      type: String,
+      validator: (value: string) => {
+        return (
+          ['primary', 'neutral', 'info', 'warning', 'danger'].indexOf(value) >=
+          0
+        )
+      },
+    },
+    /**
+     * Type of button
+     * @values button, reset, submit
+     */
+    type: {
+      type: String as PropType<ButtonHTMLAttributes['type']>,
+      default: () => 'button',
+      validator: (value: string) => {
+        return ['button', 'reset', 'submit'].indexOf(value) >= 0
+      },
+    },
+    /**
+     * size of button
+     * @values small, medium, large
+     */
+    size: {
+      type: String,
+      validator: (value: string) => {
+        return ['small', 'medium', 'large'].indexOf(value) >= 0
+      },
+    },
+  },
   setup() {
     const onClick = (event: MouseEvent): void => {
       console.log('clicou no BrButton')
@@ -18,6 +68,20 @@ export default defineComponent({
     return {
       onClick,
     }
+  },
+  computed: {
+    rootClasses() {
+      return [
+        `br-button ripple`,
+        this.size ? `br-button--${this.size}` : ``,
+        this.variant ? `br-button--${this.variant}` : ``,
+      ]
+    },
+    computedDisabled() {
+      console.log(this.disabled)
+      if (this.disabled) return true
+      return undefined
+    },
   },
 })
 </script>
